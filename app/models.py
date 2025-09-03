afee403469dcf30de53b98ff8320c3849e92abc1
devrubayet <rubayethossain2003@gmail.com>
2025-09-03 16:01:04 +0600

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,136 @@
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+
+# Create your models here.
+
+
+class Information(models.Model):
+    sitename = models.CharField(max_length=50,blank=True, null=True)
+    site_slogan = models.CharField(max_length=100,blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    logo = models.ImageField(upload_to='img/logos/')
+
+    office_address = models.TextField(blank=True, null=True)
+    office_email = models.EmailField(blank=True, null=True)
+    office_phone = models.CharField(max_length=20 , blank=True, null=True)
+
+    about_us = models.TextField(blank=True, null=True)
+    head_line = models.TextField(blank=True, null=True)
+
+
+
+    def __str__(self):
+        return self.sitename
+    
+
+
+
+class SocialLink(models.Model):
+    information = models.ForeignKey(
+        'Information', related_name='social_links', on_delete=models.CASCADE
+    )
+    platform_name = models.CharField(max_length=50,blank=True, null=True)
+    url = models.URLField()
+
+    def __str__(self):
+        return f"{self.platform_name} "
+    
+
+
+
+
+
+
+class CheckStatus(models.Model):
+    reference_number = models.CharField(max_length=100, unique=True)  # Unique reference
+    visa_name = models.CharField(max_length=100)
+    visa_status = models.CharField(max_length=100)
+    applicant_name = models.CharField(max_length=100, null=True, blank=True)
+    submission_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.reference_number
+   
+class Slider(models.Model):
+    image = models.ImageField(upload_to='carousel_images/')
+    is_active = models.BooleanField(default=True)  # For enabling/disabling images
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image {self.id}"
+
+class Our_Services_Slider(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='carousel_images/')
+    is_active = models.BooleanField(default=True)  # For enabling/disabling images
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image {self.title}"
+
+
+class VisaList(models.Model):
+    visa_name = models.CharField(max_length=100,blank=True, null=True,unique=True)
+    content = RichTextUploadingField()
+    
+
+    def __str__(self):
+        return self.visa_name
+
+class VisaSilderImage(models.Model):
+    visa = models.ForeignKey(VisaList, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='visa_images/')  # Directory for storing images
+
+    def __str__(self):
+        return f"Image for {self.visa.visa_name}"
+    
+
+
+class PackageList(models.Model):
+    package_name = models.CharField(max_length=100,blank=True, null=True,unique=True)
+    slug = models.SlugField(unique=True) 
+    content = RichTextUploadingField()
+    
+
+    def __str__(self):
+        return self.package_name
+
+class PackageSilderImage(models.Model):
+    package = models.ForeignKey(PackageList, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='package_images/')  # Directory for storing images
+
+    def __str__(self):
+        return f"Image for {self.package.package_name}"
+    
+
+class AirlinesList(models.Model):
+    airlines_name = models.CharField(max_length=100,blank=True, null=True,unique=True)
+    image = models.ImageField(upload_to='airlines_images/')  # Directory for storing images
+    
+
+    def __str__(self):
+        return self.airlines_name
+    
+
+
+class BankDetails(models.Model):
+    Bank_Name = models.CharField(max_length=100,blank=True, null=True)
+    Account_Name = models.CharField(max_length=100,blank=True, null=True)
+    Account_Number = models.CharField(max_length=100,blank=True, null=True)
+    Branch_Name = models.CharField(max_length=100,blank=True, null=True)
+    Swift_Code = models.CharField(max_length=100,blank=True, null=True)
+    Rounting_Number = models.CharField(max_length=100,blank=True, null=True)
+    
+    def __str__(self):
+        return self.Bank_Name
+
+
+class feedback(models.Model):
+    image = models.ImageField(upload_to='feedback_images/', blank=True, null=True)
+    name = models.CharField(max_length=100,blank=True, null=True)
+    bio = models.fieldName = models.CharField(max_length = 150,blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
---
+++ app/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
+from .models import *
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('track-application/', track_application, name='track_application'), 
+    # path('track-visa/', track_visa, name='track_visa'), # URL for AJAX request
+    path('visa-details/<str:visa_name>/', visa_details, name='visa_details'),
+    path('package-details/<slug:slug>/', package_details, name='package-details'),
+    path('contact-us/', contact_us, name='contact-us'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
+
+]
